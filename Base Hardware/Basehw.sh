547b6cfd7ca930f310072156e4958125e5768022
project <project@project-PC>
2017-03-03 17:43:34 +0530

    edit interface

--- Base Hardware/Basehw.sh
+++ Base Hardware/Basehw.sh
@@ -5,7 +5,9 @@ ssh-copy-id root@10.0.0.84
 ssh-copy-id root@10.0.0.85
 ssh-copy-id root@10.0.0.86
 ssh-copy-id root@10.0.0.87
-
+ssh root@10.0.0.81 'echo "Acquire::http { Proxy \"http://10.0.0.52:3128\";};" >> /etc/apt/apt.conf'
+ ssh root@10.0.0.81 'apt update'
+ ssh root@10.0.0.81 'apt -y install python' 
  ssh root@10.0.0.82 'echo "Acquire::http { Proxy \"http://10.0.0.52:3128\";};" >> /etc/apt/apt.conf'
  ssh root@10.0.0.82 'apt update'
  ssh root@10.0.0.82 'apt -y install python' 
